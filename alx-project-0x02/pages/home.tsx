44d46ef0d307c0f8ab8201ea19e0d191cc2f3cac
Mh-NOUHICoder <mohammednouhi61@gmail.com>
2025-09-26 22:38:46 +0100

    Add common folder

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from "@/components/layout/Header";
-import Card from "@/components/Card";
+import Card from "@/components/common/Card";
 import { useCards } from "@/hooks/useCards";
 
 const HomePage: React.FC = () => {
