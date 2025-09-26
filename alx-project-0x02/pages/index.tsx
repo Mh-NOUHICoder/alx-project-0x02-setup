3585efc43bac7c998e9f050b43af08ecf125487a
Mh-NOUHICoder <mohammednouhi61@gmail.com>
2025-09-26 12:07:50 +0100

    Convert alx-project-0x02 to regular folder

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,14 @@
+// pages/index.tsx
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Welcome to alx-project-0x02 🚀
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
